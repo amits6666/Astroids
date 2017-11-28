a9515842efa718541a4bb9ee3a9ca1be3280e323
Maestroxr <maestroeyal@gmail.com>
2017-11-28 20:28:45 +0200

    Ignore logs file fix in gitignore, also TestScene added

---
+++ workers/unity/Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotatePlayer();
+	}
+
+    public void RotatePlayer()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.LookAt(hit.point);
+            transform.Rotate(90, 0, 0);
+          
+        }
+    }
+}
